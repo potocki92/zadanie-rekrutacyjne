861921fc40025f0f024ebd9ad1eb4bab3ea8d231
potocki92 <mateusz.potocki92@gmail.com>
2023-08-19 19:04:14 +0200

    mobile button/fixed
    Zmiana js z przyciskiem rozwijającym nawigacje w mobilce

--- js/input-filled.js
+++ js/input-filled.js
@@ -9,18 +9,15 @@ searchInput.addEventListener("input", () => {
 
 const resizeObserver = new ResizeObserver((entries) => {
   for (const entry of entries) {
-    if (entry.target === searchInput && entry.contentRect.width === 0) {
-      searchInput.classList.add("no-outline");
-    } else {
-      searchInput.classList.remove("no-outline");
-    }
+    entry.target === searchInput && entry.contentRect.width === 0
+      ? searchInput.classList.add("no-outline")
+      : searchInput.classList.remove("no-outline");
   }
 });
 
 resizeObserver.observe(searchInput);
 
-
-// Mobile 
+// Mobile
 const menuToggle = document.getElementById("menu-toggle");
 const nav = document.querySelector("nav");
 const links = document.getElementById("nav-links");
@@ -28,16 +25,26 @@ const links = document.getElementById("nav-links");
 menuToggle.addEventListener("click", () => {
   menuToggle.classList.toggle("active");
   nav.classList.toggle("show");
-  links.classList.toggle("hidden");
 });
 
-menuToggle.addEventListener("click", () => {
-  menuToggle.classList.toggle("active");
+const header = document.querySelector("header");
 
+window.addEventListener("scroll", () => {
+  if (window.innerWidth <= 768) {
+    if (window.scrollY > 0) {
+      header.classList.add("fixed", "top-0", "bg-white", "shadow-md");
+    } else {
+      header.classList.remove("fixed", "top-0", "bg-white", "shadow-md");
+    }
+  }
+});
+
+// Dodaj nasłuch szerokości okna
+window.addEventListener("resize", () => {
   if (window.innerWidth >= 768) {
-    nav.classList.toggle("show");
-    links.classList.toggle("hidden");
-  } else {
-    nav.classList.toggle("show");
+    menuToggle.classList.remove("active");
+    nav.classList.remove("show");
+
+    header.classList.remove("fixed", "top-0", "bg-white", "shadow-md");
   }
 });
