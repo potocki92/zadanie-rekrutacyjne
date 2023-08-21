6c4ea7efb429010756c848f112125b17915f299e
potocki92 <mateusz.potocki92@gmail.com>
2023-08-21 14:36:29 +0200

    Updates

--- index.969649e4.js
+++ index.969649e4.js
@@ -1,24 +1,22 @@
-// Poniżej znajduje się przykład kodu JavaScript dołączanego do twojego pliku HTML
 document.addEventListener("DOMContentLoaded", function() {
     const h1 = document.querySelector(".hero h1");
     const span = document.querySelector(".hero span");
     const img = document.querySelector("#hero img");
-    console.log(img);
     const content = [
         {
             text: "Nowoczesna aranżacja Twojego ogrodu",
             spanText: "Marka GiardDesign to wieloletnie doświadczenie i wysoka estetyka realizacji. Oferujemy kompleksowy zakres usług z indywidualnym podejściem do każdego projektu.",
-            imgSrcset: "/Photo.e147f6fd.jpg 1x, /Photo@2x.afe82740.jpg 2x"
+            imgSrcset: "/zadanie-rekrutacyjne/Photo.e147f6fd.jpg 1x, /zadanie-rekrutacyjne/Photo@2x.afe82740.jpg 2x"
         },
         {
             text: "Zielone piękno Twojego ogrodu",
             spanText: "Nasza firma zajmuje się profesjonalną aranżacją ogrod\xf3w, tworząc wyjątkowe przestrzenie pełne natury i harmonii.",
-            imgSrcset: "/hero-garden.fa6a9902.jpg 1x, /hero-garden.fa6a9902.jpg 2x"
+            imgSrcset: "/zadanie-rekrutacyjne/hero-garden.fa6a9902.jpg 1x, /zadanie-rekrutacyjne/hero-garden.fa6a9902.jpg 2x"
         },
         {
             text: "Tw\xf3j raj na ziemi",
             spanText: "Stworzymy dla Ciebie wymarzony ogr\xf3d, gdzie możesz odpocząć, cieszyć się pięknem roślin i spędzać czas na łonie natury.",
-            imgSrcset: "/hero-garden-2.22fb90a1.jpg 1x, /hero-garden-2.22fb90a1.jpg 2x"
+            imgSrcset: "/zadanie-rekrutacyjne/hero-garden-2.22fb90a1.jpg 1x, /zadanie-rekrutacyjne/hero-garden-2.22fb90a1.jpg 2x"
         }
     ];
     let currentIndex = 0;
@@ -27,6 +25,20 @@ document.addEventListener("DOMContentLoaded", function() {
         span.textContent = content[currentIndex].spanText;
         img.srcset = content[currentIndex].imgSrcset;
     }
+    function handleTouchStart(event) {
+        startX = event.touches[0].clientX;
+    }
+    function handleTouchEnd(event) {
+        if (typeof startX === "undefined") return;
+        const endX = event.changedTouches[0].clientX;
+        const deltaX = startX - endX;
+        if (deltaX > 0) currentIndex = (currentIndex + 1) % content.length;
+        else if (deltaX < 0) currentIndex = (currentIndex - 1 + content.length) % content.length;
+        updateContent();
+        startX = undefined;
+    }
+    document.addEventListener("touchstart", handleTouchStart);
+    document.addEventListener("touchend", handleTouchEnd);
     document.getElementById("before").addEventListener("click", function() {
         currentIndex = (currentIndex - 1 + content.length) % content.length;
         updateContent();
