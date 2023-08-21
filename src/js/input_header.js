e1f581920f0bf38899f131a2837a886950dac20a
potocki92 <mateusz.potocki92@gmail.com>
2023-08-21 18:46:58 +0200

    navigation/update
    Zmiana wyglądu oraz zachowanie nawigacji w momencie kiedy użytkownik obsługuje stronę na mobilce

--- src/js/input_header.js
+++ src/js/input_header.js
@@ -1,4 +1,6 @@
-// Input search
+/**
+ * Funkcja inicjalizująca zachowanie interakcji na stronie dla urządzeń mobilnych.
+ */
 (function () {
   const searchInput = document.getElementById("search-input");
 
@@ -8,6 +10,9 @@
       : searchInput.classList.remove("input-filled");
   });
 
+  /**
+   * Obserwuje zmiany szerokości pola wyszukiwania.
+   */
   const resizeObserver = new ResizeObserver((entries) => {
     for (const entry of entries) {
       entry.target === searchInput && entry.contentRect.width === 0
@@ -18,35 +23,65 @@
 
   resizeObserver.observe(searchInput);
 
-  // Mobile
+  /**
+   * Obsługuje menu mobilne oraz nawigację.
+   */
   const menuToggle = document.getElementById("menu-toggle");
   const nav = document.querySelector("nav");
-  const links = document.getElementById("nav-links");
+  const links = document.getElementById("myTopnav");
+  const menuItems = document.querySelectorAll("#myTopnav a");
+  const heroLogo = document.getElementById("hero-logo");
 
+  console.log(links, menuItems);
   menuToggle.addEventListener("click", () => {
     menuToggle.classList.toggle("active");
     nav.classList.toggle("show");
   });
 
+  menuItems.forEach((item, index, array) => {
+    if (index === 0) {
+      return; // Omiń pierwszy element oraz ostatni element
+    }
+    item.addEventListener("click", () => {
+      if (window.innerWidth <= 768) {
+        nav.classList.remove("show");
+        menuToggle.classList.remove("active");
+      }
+    });
+  });
+
+  heroLogo.addEventListener("click", () => {
+    if (window.innerWidth <= 768) {
+      nav.classList.remove("show");
+      menuToggle.classList.remove("active");
+    }
+  });
   const header = document.querySelector("header");
 
+  /**
+   * Obsługuje efekt związany ze zmianą pozycji scrolla.
+   */
   window.addEventListener("scroll", () => {
     if (window.innerWidth <= 768) {
-      if (window.scrollY > 0) {
-        header.classList.add("fixed", "top-0", "bg-white", "shadow-md");
-      } else {
-        header.classList.remove("fixed", "top-0", "bg-white", "shadow-md");
-      }
+      window.scrollY > 0
+        ? header.classList.add("fixed", "top-0", "bg-white", "shadow-md")
+        : header.classList.remove("fixed", "top-0", "bg-white", "shadow-md");
     }
   });
 
-  // Dodaj nasłuch szerokości okna
+  /**
+   * Nasłuchuje zmiany szerokości okna przeglądarki.
+   */
   window.addEventListener("resize", () => {
     if (window.innerWidth >= 768) {
       menuToggle.classList.remove("active");
       nav.classList.remove("show");
-
       header.classList.remove("fixed", "top-0", "bg-white", "shadow-md");
     }
   });
+
+  /**
+   * Obsługuje responsywną nawigację mobilną.
+   */
+  const topnav = document.getElementById("myTopnav");
 })();
