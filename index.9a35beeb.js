00ab9f158b4dfe7abaf7d57e547563cf6e5a3dc5
potocki92 <mateusz.potocki92@gmail.com>
2023-08-21 16:53:56 +0200

    Updates

--- index.9a35beeb.js
+++ index.9a35beeb.js
@@ -1,15 +1,19 @@
-// Input search
-(function() {
+/**
+ * Funkcja inicjalizująca zachowanie interakcji na stronie dla urządzeń mobilnych.
+ */ (function() {
     const searchInput = document.getElementById("search-input");
     searchInput.addEventListener("input", ()=>{
         searchInput.value.trim() !== "" ? searchInput.classList.add("input-filled") : searchInput.classList.remove("input-filled");
     });
-    const resizeObserver = new ResizeObserver((entries)=>{
+    /**
+   * Obserwuje zmiany szerokości pola wyszukiwania.
+   */ const resizeObserver = new ResizeObserver((entries)=>{
         for (const entry of entries)entry.target === searchInput && entry.contentRect.width === 0 ? searchInput.classList.add("no-outline") : searchInput.classList.remove("no-outline");
     });
     resizeObserver.observe(searchInput);
-    // Mobile
-    const menuToggle = document.getElementById("menu-toggle");
+    /**
+   * Obsługuje menu mobilne oraz nawigację.
+   */ const menuToggle = document.getElementById("menu-toggle");
     const nav = document.querySelector("nav");
     const links = document.getElementById("nav-links");
     const menuItems = document.querySelectorAll("#nav-links li");
@@ -20,7 +24,7 @@
         nav.classList.toggle("show");
     });
     menuItems.forEach((item, index)=>{
-        if (index === 0) return;
+        if (index === 0 || index === array.length - 1) return; // Omiń pierwszy element
         item.addEventListener("click", ()=>{
             if (window.innerWidth <= 768) {
                 links.classList.add("invisible");
@@ -37,11 +41,14 @@
         }
     });
     const header = document.querySelector("header");
-    window.addEventListener("scroll", ()=>{
+    /**
+   * Obsługuje efekt związany ze zmianą pozycji scrolla.
+   */ window.addEventListener("scroll", ()=>{
         if (window.innerWidth <= 768) window.scrollY > 0 ? header.classList.add("fixed", "top-0", "bg-white", "shadow-md") : header.classList.remove("fixed", "top-0", "bg-white", "shadow-md");
     });
-    // Dodaj nasłuch szerokości okna
-    window.addEventListener("resize", ()=>{
+    /**
+   * Nasłuchuje zmiany szerokości okna przeglądarki.
+   */ window.addEventListener("resize", ()=>{
         if (window.innerWidth >= 768) {
             menuToggle.classList.remove("active");
             nav.classList.remove("show");
